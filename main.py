21f270048249ba052d66c165bbaed5eb7ad3c7ed
Adam <asage@hmc.edu>
2023-11-14 16:23:18 -0800

    added buggy ray generation

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import time
 
 # help from: https://github.com/PyOCL/pyopencl-examples
 
-SCREEN_WIDTH = 3
-SCREEN_HEIGHT = 3
+SCREEN_WIDTH = 10
+SCREEN_HEIGHT = 5
 
-INPUT_SIZE = 9
 OUTPUT_SIZE = SCREEN_HEIGHT * SCREEN_WIDTH
 DEBUG_BUFFER_SIZE = 10000
 
@@ -22,19 +21,21 @@ if __name__ == '__main__':
 
     # light type struct
     light_type = np.dtype([('position', vector_type),
-                           ('r', np.uint32), ('g', np.uint32), ('b', np.uint32),
+                           ('r', np.float32), ('g', np.float32), ('b', np.float32),
                            ('intensity', np.float32)])
 
     # -1 to 1 pixel position struct
     pixel_pos_type = np.dtype([('pix_x', np.float32), ('pix_y', np.float32)])
 
     # output pixel color struct
-    pixel_color_type = np.dtype([('r', np.uint32), ('g', np.uint32), ('b', np.uint32)])
+    pixel_color_type = np.dtype([('r', np.float32), ('g', np.float32), ('b', np.float32)])
 
     # camera data struct
     camera_data_type = np.dtype([('position', vector_type),
                                  ('right', vector_type),
-                                 ('up', vector_type)])
+                                 ('up', vector_type),
+                                 ('forward', vector_type),  # note: only including this cause it's faster to do once
+                                 ])
 
     # initialize triangle data, 3 indexes for vertices, 3 floats for normal vector, 1 material index
     triangle_data = np.array([(0, 1, 2, (0.0, 0.0, -1.0), 1), (0, 3, 4, (0.0, 0.0, -1.0), 1)],
@@ -54,7 +55,8 @@ if __name__ == '__main__':
     light_data = np.array([((0.0, 0.5, 0.0), 255, 255, 255, 1.0)], dtype=light_type)
 
     # initialize camera data array
-    camera_data = np.array([((0.0, 0.0, 0.0), (1.0, 0.0, 0.0), (0.0, 1.0, 0.0))], dtype=camera_data_type)
+    camera_data = np.array([((0.0, 0.0, 0.0), (1.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 1.0, 0.0))],
+                           dtype=camera_data_type)
 
     # load program from cl source file
     f = open('trace.cl', 'r', encoding='utf-8')
@@ -62,7 +64,7 @@ if __name__ == '__main__':
     f.close()
 
     # prepare data
-    matrix = np.random.randint(low=1, high=101, dtype=np.uint32, size=INPUT_SIZE)
+    matrix = np.random.randint(low=1, high=101, dtype=np.uint32, size=OUTPUT_SIZE)
 
     # prepare memory for final answer from OpenCL
     out = np.empty(OUTPUT_SIZE, dtype=pixel_color_type)
@@ -71,7 +73,6 @@ if __name__ == '__main__':
     # debug is a string of '$$$$$$$0'
     debug = np.full(DEBUG_BUFFER_SIZE, 36, dtype=np.uint8)
     debug[DEBUG_BUFFER_SIZE-1] = 0
-    print(debug)
     # create context
     ctx = cl.create_some_context()
 
@@ -86,25 +87,24 @@ if __name__ == '__main__':
     pixel_pos_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=pix_data)
     # prepare device memory for output
     out_buf = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, out.nbytes)
-    debug_buf = cl.Buffer(ctx, cl.mem_flags.KERNEL_READ_AND_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=debug)
+    debug_buf = cl.Buffer(ctx, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=debug)
 
     # compile kernel code
     prg = cl.Program(ctx, kernels).build()
     time_kernel_compilation = time.time()
 
     # execute kernel programs
-    evt = prg.trace_rays(queue, (INPUT_SIZE,), (1,), triangle_buf, vertex_buf, light_buf,
+    evt = prg.trace_rays(queue, (OUTPUT_SIZE,), (1,), triangle_buf, vertex_buf, light_buf,
                          camera_buf, pixel_pos_buf, out_buf, debug_buf)
     # wait for kernel executions
     evt.wait()
 
     cl.enqueue_copy(queue, out, out_buf).wait()
     cl.enqueue_copy(queue, debug, debug_buf).wait()
-    print(debug)
     debug_string = ""
     i = 0
     while debug[i] != 36:
         debug_string += chr(debug[i])
         i += 1
     print(debug_string)
-
+    print(out)
--- trace.cl
+++ trace.cl
@@ -2,45 +2,54 @@ typedef struct {
     float x;
     float y;
     float z;
-} vector_type;
+} vector;
+
+typedef struct {
+    vector pos;
+    vector dir;
+} ray;
+
 
 typedef struct {
     uint a;
     uint b;
     uint c;
-    vector_type normal;
+    vector normal;
     int mat;
-} triangle_type;
+} triangle;
 
 typedef struct {
-    vector_type position;
-    uint r;
-    uint g;
-    uint b;
+    vector position;
+    float r;
+    float g;
+    float b;
     float intensity;
-} light_type;
+} light;
 
 typedef struct {
     float x;
     float y;
-} pixel_pos_type;
+} pixel_pos;
 
 typedef struct {
-    uint r;
-    uint g;
-    uint b;
-} pixel_color_type;
+    float r;
+    float g;
+    float b;
+} pixel_color;
 
 typedef struct {
-    vector_type position;
-    vector_type right;
-    vector_type up;
-} camera_data_type;
+    vector position;
+    vector right;
+    vector up;
+    vector forwards;
+    float focal_dist;
+} camera_data;
+
 
 void print(__constant char* str, __global char* debug_buf, int debug_id);
 
 // this is a kind of a janky debug function, we can fix it later
-// just dont write any $ signs in your debugging I guess.
+// just don't write any $ signs in your debugging I guess.
 // this will get really slow if we print out a lot at a time
 void print(__constant char* str, __global char* debug_buf, int debug_id) {
     int DEBUG_ID = 4;
@@ -62,24 +71,59 @@ void print(__constant char* str, __global char* debug_buf, int debug_id) {
     if (start != 0) {
         debug_buf[start] = 10;
     }
+    free(str);
 }
 
+float vdot(__constant vector* v1, __constant vector* v2);
 
+float vdot(__constant vector* v1, __constant vector* v2) {
+    return v1->x * v2->x + v1->y + v2->y + v1->z * v2->z;
+}
 
-__kernel void trace_rays(__global triangle_type* tris,
-                         __global vector_type* vertices,
-                         __global light_type* lights,
-                         __global camera_data_type* camera,
-                         __global pixel_pos_type* pixels,
-                         __global pixel_color_type* out,
+void vnormalize(__private vector* v);
+
+void vnormalize(__private vector* v) {
+    float mag = 1; //magnitude(v->x);
+    v->x = v->x / mag;
+    v->y = v->y / mag;
+    v->z = v->z / mag;
+}
+
+float vmagnitude(__constant vector* v);
+
+float vmagnitude(__constant vector* v) {
+    return sqrt(v->x * v->x + v->y + v->y + v->z * v->z);
+}
+
+__kernel void trace_rays(__global triangle* tris,
+                         __global vector* vertices,
+                         __global light* lights,
+                         __global camera_data* camera,
+                         __global pixel_pos* pixels,
+                         __global pixel_color* out,
                          __global char* debug) {
 
-    int global_id = get_global_id(0);
-
-    print("Abcdef", debug, global_id);
+    __private int global_id = get_global_id(0);
 
 
-    out[global_id].r = global_id;
-    out[global_id].g = 0;
-    out[global_id].b = 0;
+    __private pixel_pos screen_coords = pixels[global_id];
+    // forwards vector
+    __private vector f = camera->forwards;
+    // right vector
+    __private vector u = camera->up;
+    // up vector
+    __private vector r = camera->right;
+
+    // initialize ray
+    __private vector ray_dir = {f.x + u.x * screen_coords.y + r.x * screen_coords.x,
+                      f.y + u.y * screen_coords.y + r.y * screen_coords.x,
+                      f.z + u.z * screen_coords.y + r.z * screen_coords.x
+                     };
+    vnormalize(&ray_dir);
+
+
+
+    out[global_id].r = ray_dir.x;
+    out[global_id].g = ray_dir.y;
+    out[global_id].b = ray_dir.z;
 }
