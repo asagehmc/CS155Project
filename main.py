b92a11263b5d75eee8e588db834499efd6242283
Adam <asage@hmc.edu>
2023-11-14 21:18:22 -0800

    ray tracing!! (all white)

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import time
 
 # help from: https://github.com/PyOCL/pyopencl-examples
 
-SCREEN_WIDTH = 4
-SCREEN_HEIGHT = 2
+SCREEN_WIDTH = 3
+SCREEN_HEIGHT = 3
 
 OUTPUT_SIZE = SCREEN_HEIGHT * SCREEN_WIDTH
-DEBUG_BUFFER_SIZE = 10000
 
 if __name__ == '__main__':
     # vector struct
@@ -24,6 +23,10 @@ if __name__ == '__main__':
                            ('r', np.float32), ('g', np.float32), ('b', np.float32),
                            ('intensity', np.float32)])
 
+    # world data struct
+    world_data_type = np.dtype([('num_tris', np.int32),
+                                ('num_lights', np.int32)])
+
     # -1 to 1 pixel position struct
     pixel_pos_type = np.dtype([('pix_x', np.float32), ('pix_y', np.float32)])
 
@@ -36,17 +39,25 @@ if __name__ == '__main__':
                                  ('up', vector_type),
                                  ('forward', vector_type),  # note: only including this cause it's faster to do once
                                  ])
+    # TEMPORARILY REMOVED TRIANGLE TEST SET
+    # # initialize triangle data, 3 indexes for vertices, 3 floats for normal vector, 1 material index
+    # triangle_data = np.array([(0, 1, 2, (0.0, 0.0, -1.0), 1), (0, 3, 4, (0.0, 0.0, -1.0), 1)],
+    #                          dtype=triangle_type)
+    #
+    # # initialize triangle vertices (can be pointed to by multiple triangles)
+    # vertex_data = np.array([[0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [-1.0, 1.0, 1.0], [0.0, -1.0, 15.0], [1.0, -1.0, 1.0]],
+    #                        dtype=vector_type)
 
-    # initialize triangle data, 3 indexes for vertices, 3 floats for normal vector, 1 material index
-    triangle_data = np.array([(0, 1, 2, (0.0, 0.0, -1.0), 1), (0, 3, 4, (0.0, 0.0, -1.0), 1)],
+    triangle_data = np.array([(0, 1, 2, (0.0, 1.0, 0.0), 1)],
                              dtype=triangle_type)
 
     # initialize triangle vertices (can be pointed to by multiple triangles)
-    vertex_data = np.array([[0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [-1.0, 1.0, 1.0], [0.0, -1.0, 15.0], [1.0, -1.0, 1.0]],
+    vertex_data = np.array([(0.0, -1, 1.0), (1.0, -1, 1.0), (1.0, -1, 0.0)],
                            dtype=vector_type)
+    print(vertex_data)
 
     # initialize pixel data array, an array of indexes for each pixel's position on the screen, starting at top right.
-    frac = SCREEN_HEIGHT/SCREEN_WIDTH
+    frac = SCREEN_HEIGHT / SCREEN_WIDTH
     pix_data = np.array([(((i % SCREEN_WIDTH) + 0.5) / SCREEN_WIDTH * 2 - 1,
                           frac - ((i // SCREEN_WIDTH) + 0.5) / SCREEN_WIDTH * 2)
                          for i in range(SCREEN_WIDTH * SCREEN_HEIGHT)],
@@ -54,6 +65,9 @@ if __name__ == '__main__':
 
     # initialize light data array
     light_data = np.array([((0.0, 0.5, 0.0), 255, 255, 255, 1.0)], dtype=light_type)
+
+    # initialize world data
+    world_data = np.array([(triangle_data.shape[0], light_data.shape[0])], dtype=world_data_type)
 
     # initialize camera data array
     # position, right, up, forward
@@ -78,11 +92,13 @@ if __name__ == '__main__':
     queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
 
     # prepare device memory for input
-    triangle_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=vertex_data)
-    vertex_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=pix_data)
+    triangle_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=triangle_data)
+    vertex_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=vertex_data)
     light_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=light_data)
     camera_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=camera_data)
     pixel_pos_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=pix_data)
+    world_data_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=world_data)
+
     # prepare device memory for output
     out_buf = cl.Buffer(ctx, cl.mem_flags.WRITE_ONLY, out.nbytes)
 
@@ -92,7 +108,7 @@ if __name__ == '__main__':
 
     # execute kernel programs
     evt = prg.trace_rays(queue, (OUTPUT_SIZE,), (1,), triangle_buf, vertex_buf, light_buf,
-                         camera_buf, pixel_pos_buf, out_buf)
+                         camera_buf, pixel_pos_buf, world_data_buf, out_buf)
     # wait for kernel executions
     evt.wait()
 
--- trace.cl
+++ trace.cl
@@ -9,6 +9,10 @@ typedef struct {
     vector dir;
 } ray;
 
+typedef struct {
+    int num_tris;
+    int num_lights;
+} world_data;
 
 typedef struct {
     uint a;
@@ -45,31 +49,45 @@ typedef struct {
     float focal_dist;
 } camera_data;
 
-float vdot(__constant vector* v1, __constant vector* v2);
-void vnormalize(__private vector* v);
-float vmagnitude(__private vector* v);
+float ppdot(__private vector* v1, __private vector* v2);
+float pgdot(__private vector* v1, __global vector* v2);
+
+vector gpminus(__global vector* v1, __private vector* v2);
+void pnormalize(__private vector* v);
+float pmagnitude(__private vector* v);
 
 
-float vdot(__constant vector* v1, __constant vector* v2) {
-    return v1->x * v2->x + v1->y + v2->y + v1->z * v2->z;
+
+float ppdot(__private vector* v1, __private vector* v2) {
+    return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
+}
+float pgdot(__private vector* v1, __global vector* v2) {
+    return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
 }
 
-float vmagnitude(__private vector* v) {
+float pmagnitude(__private vector* v) {
     return sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
 }
 
-void vnormalize(__private vector* v) {
-    float mag = vmagnitude(v);
+void pnormalize(__private vector* v) {
+    float mag = pmagnitude(v);
     v->x = v->x / mag;
     v->y = v->y / mag;
     v->z = v->z / mag;
 }
+
+vector gpminus(__global vector* v1, __private vector* v2) {
+    vector v = {v1->x - v2->x, v1->y - v2->y, v1->z - v2->z};
+    return v;
+}
+
 
 __kernel void trace_rays(__global triangle* tris,
                          __global vector* vertices,
                          __global light* lights,
                          __global camera_data* camera,
                          __global pixel_pos* pixels,
+                         __global world_data* world,
                          __global pixel_color* out
                         ) {
 
@@ -89,10 +107,46 @@ __kernel void trace_rays(__global triangle* tris,
                       f.y + u.y * screen_coords.y + r.y * screen_coords.x,
                       f.z + u.z * screen_coords.y + r.z * screen_coords.x
                      };
-    vnormalize(&ray_dir);
+    pnormalize(&ray_dir);
+    __private ray ray_cast = {camera->position, ray_dir};
+    int closest_triangle = -1;
+    float closest_hit = INFINITY;
+    for (int i = 0; i < world->num_tris; ++i) {
+        // if ray & plane are not parallel
+        if (pgdot(&ray_dir, &(tris[i].normal)) != 0) {
+            __private vector point_minus_direction = gpminus(&(vertices[tris[i].a]), &(ray_cast.pos));
+            float d = pgdot(&point_minus_direction, &(tris[i].normal)) /
+                      pgdot(&(ray_cast.dir), &(tris[i].normal));
+            if (global_id == 7) {
+                printf("L: %f %f %f", ray_dir.x, ray_dir.y, ray_dir.z);
+                printf("P0: %f %f %f", vertices[tris[i].a].x, vertices[tris[i].a].y, vertices[tris[i].a].z);
+                printf("L0: %f %f %f", ray_cast.pos.x, ray_cast.pos.y, ray_cast.pos.z);
+                printf("n: %f %f %f", tris[i].normal.x, tris[i].normal.y, tris[i].normal.z);
+
+                printf("P0 - L0: %f %f %f", point_minus_direction.x, point_minus_direction.y, point_minus_direction.z);
+
+                printf("dot(P0 - L0, N), %f", pgdot(&point_minus_direction, &(tris[i].normal)));
+//                printf("P0 - L0: %f %f %f", point_minus_direction.x, point_minus_direction.y, point_minus_direction.z);
 
 
-    out[global_id].r = ray_dir.x;
-    out[global_id].g = ray_dir.y;
-    out[global_id].b = ray_dir.z;
+
+
+                printf("d for %f", d);
+            }
+            if (d >= 0 && d < closest_hit) {
+                printf("HIT in %i, %f", global_id, d);
+                closest_hit = d;
+                closest_triangle = 1;
+            }
+        }
+    }
+    //__global vector v1_global = ;
+    //__private vector v1 = {v1_global.x, v1_global.y, v1_global.z};
+    if (closest_triangle != -1) {
+        out[global_id].r = 1;
+        out[global_id].g = 1;
+        out[global_id].b = 1;
+    }
+
+
 }
