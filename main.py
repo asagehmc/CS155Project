58646f3b222328dced81f037703b64b2d6f2f6ab
Adam <asage@hmc.edu>
2023-11-14 23:15:16 -0800

    got spectral light working

--- main.py
+++ main.py
@@ -20,48 +20,39 @@ if __name__ == '__main__':
     # vector struct
     vector_type = np.dtype([('x', np.float32), ('y', np.float32), ('z', np.float32)])
 
+    # initialize triangle vertices (can be pointed to by multiple triangles)
+    vertex_data = np.array([(0.0, -1, 1.0), (1.0, -1, 1.0), (1.0, -1, 0.0)],
+                           dtype=vector_type)
+
     # triangle struct, 3 vertex indexes, 3 floats for normal vector, 1 material index
     triangle_type = np.dtype([('v1', np.uint32), ('v2', np.uint32), ('v3', np.uint32),
                               ('normal', vector_type),
                               ('mat', np.uint32)])
 
+    # initialize triangle data, 3 indexes for vertices, 3 floats for normal vector, 1 material index
+    triangle_data = np.array([(0, 1, 2, (0.0, 1.0, 0.0), 0)],
+                             dtype=triangle_type)
+    print(triangle_data)
     # light type struct
     light_type = np.dtype([('position', vector_type),
-                           ('r', np.float32), ('g', np.float32), ('b', np.float32),
+                           ("color", vector_type),
                            ('intensity', np.float32)])
+
+    # initialize light data array
+    light_data = np.array([((0.0, 0.5, 0.0), (1.0, 1.0, 1.0), 1.0)], dtype=light_type)
 
     # world data struct
     world_data_type = np.dtype([('num_tris', np.int32),
                                 ('num_lights', np.int32),
-                                ('world_ambient_intensity', np.float)])
+                                ('world_ambient_color', vector_type),
+                                ('world_ambient_intensity', np.float32)])
+
+    # initialize world data
+    world_data = np.array([(triangle_data.shape[0], light_data.shape[0],
+                            (1, 1, 1), 1.0)], dtype=world_data_type)
 
     # -1 to 1 pixel position struct
     pixel_pos_type = np.dtype([('pix_x', np.float32), ('pix_y', np.float32)])
-
-    # material struct
-    # pixel_color_type = np.dtype([('r', np.int8), ('g', np.int8), ('b', np.int8)])
-
-    # camera data struct
-    camera_data_type = np.dtype([('position', vector_type),
-                                 ('right', vector_type),
-                                 ('up', vector_type),
-                                 ('forward', vector_type),  # note: only including this cause it's faster to do once
-                                 ])
-    # TEMPORARILY REMOVED TRIANGLE TEST SET
-    # # initialize triangle data, 3 indexes for vertices, 3 floats for normal vector, 1 material index
-    # triangle_data = np.array([(0, 1, 2, (0.0, 0.0, -1.0), 1), (0, 3, 4, (0.0, 0.0, -1.0), 1)],
-    #                          dtype=triangle_type)
-    #
-    # # initialize triangle vertices (can be pointed to by multiple triangles)
-    # vertex_data = np.array([[0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [-1.0, 1.0, 1.0], [0.0, -1.0, 15.0], [1.0, -1.0, 1.0]],
-    #                        dtype=vector_type)
-
-    triangle_data = np.array([(0, 1, 2, (0.0, 1.0, 0.0), 1)],
-                             dtype=triangle_type)
-
-    # initialize triangle vertices (can be pointed to by multiple triangles)
-    vertex_data = np.array([(0.0, -1, 1.0), (1.0, -1, 1.0), (1.0, -1, 0.0)],
-                           dtype=vector_type)
 
     # initialize pixel data array, an array of indexes for each pixel's position on the screen, starting at top right.
     frac = SCREEN_HEIGHT / SCREEN_WIDTH
@@ -70,11 +61,19 @@ if __name__ == '__main__':
                          for i in range(SCREEN_WIDTH * SCREEN_HEIGHT)],
                         dtype=pixel_pos_type)
 
-    # initialize light data array
-    light_data = np.array([((0.0, 0.5, 0.0), 255, 255, 255, 1.0)], dtype=light_type)
+    # material struct
+    material_type = np.dtype([('ambient_color', vector_type),
+                              ('diffuse_color', vector_type),
+                              ('specular_color', vector_type),
+                              ('specular_power', np.int32)])
 
-    # initialize world data
-    world_data = np.array([(triangle_data.shape[0], light_data.shape[0])], dtype=world_data_type)
+    material_data = np.array([((0.2, 0.2, 0.2), (0.6, 0.6, 0.6), (0.2, 0.2, 0.2), 30)], dtype=material_type)
+
+    # camera data struct
+    camera_data_type = np.dtype([('position', vector_type),
+                                 ('right', vector_type),
+                                 ('up', vector_type),
+                                 ('forward', vector_type)])  # note: only including this cause it's faster to do once
 
     # initialize camera data array
     # position, right, up, forward
@@ -103,6 +102,7 @@ if __name__ == '__main__':
     vertex_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=vertex_data)
     light_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=light_data)
     camera_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=camera_data)
+    material_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=material_data)
     pixel_pos_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=pix_data)
     world_data_buf = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=world_data)
 
@@ -115,7 +115,7 @@ if __name__ == '__main__':
 
     # execute kernel programs
     evt = prg.trace_rays(queue, (OUTPUT_SIZE,), (1,), triangle_buf, vertex_buf, light_buf,
-                         camera_buf, pixel_pos_buf, world_data_buf, out_buf)
+                         camera_buf, material_buf, pixel_pos_buf, world_data_buf, out_buf)
     # wait for kernel executions
     evt.wait()
 
--- trace.cl
+++ trace.cl
@@ -12,8 +12,17 @@ typedef struct {
 typedef struct {
     int num_tris;
     int num_lights;
+    vector world_ambient_color;
     float world_ambient_intensity;
 } world_data;
+
+typedef struct {
+    vector ambient_color;
+    vector diffuse_color;
+    vector specular_color;
+    int world_ambient_intensity;
+} material;
+
 
 typedef struct {
     uint a;
@@ -25,9 +34,7 @@ typedef struct {
 
 typedef struct {
     vector position;
-    float r;
-    float g;
-    float b;
+    vector color;
     float intensity;
 } light;
 
@@ -52,11 +59,13 @@ typedef struct {
 
 float ppdot(__private vector* v1, __private vector* v2);
 float pgdot(__private vector* v1, __global vector* v2);
-
-vector gpminus(__global vector* v1, __private vector* v2);
+vector ppminus(__private vector* v1, __private vector* v2);
 void pnormalize(__private vector* v);
 float pmagnitude(__private vector* v);
-
+vector gpmult(__global vector* v1, __private vector* v2);
+vector ppmult(__private vector* v1, __private vector* v2);
+vector pCmult(__private vector* v1, float f);
+vector ppsum(__private vector* v1, __private vector* v2);
 
 
 float ppdot(__private vector* v1, __private vector* v2) {
@@ -77,9 +86,29 @@ void pnormalize(__private vector* v) {
     v->z = v->z / mag;
 }
 
-vector gpminus(__global vector* v1, __private vector* v2) {
+vector ppminus(__private vector* v1, __private vector* v2) {
     vector v = {v1->x - v2->x, v1->y - v2->y, v1->z - v2->z};
     return v;
+}
+
+vector gpmult(__global vector* v1, __private vector* v2) {
+    __private vector v = {v1->x * v2->x, v1->y * v2->y, v1->z * v2->z};
+    return v;
+}
+
+vector ppmult(__private vector* v1, __private vector* v2) {
+    __private vector v = {v1->x * v2->x, v1->y * v2->y, v1->z * v2->z};
+    return v;
+}
+
+vector pCmult(__private vector* v, float f) {
+    __private vector vout = {v->x * f, v->y * f, v->z * f};
+    return vout;
+}
+
+vector ppsum(__private vector* v1, __private vector* v2) {
+    __private vector vout = {v1->x + v2->x, v1->y + v2->y, v1->z + v2->z};
+    return vout;
 }
 
 
@@ -87,6 +116,7 @@ __kernel void trace_rays(__global triangle* tris,
                          __global vector* vertices,
                          __global light* lights,
                          __global camera_data* camera,
+                         __global material* materials,
                          __global pixel_pos* pixels,
                          __global world_data* world,
                          __global pixel_color* out
@@ -112,26 +142,62 @@ __kernel void trace_rays(__global triangle* tris,
     __private ray ray_cast = {camera->position, ray_dir};
     int closest_triangle = -1;
     float closest_hit = INFINITY;
+    // find closest triangle
     for (int i = 0; i < world->num_tris; ++i) {
         // if ray & plane are not parallel
         if (pgdot(&ray_dir, &(tris[i].normal)) != 0) {
-            __private vector point_minus_direction = gpminus(&(vertices[tris[i].a]), &(ray_cast.pos));
+            vector point_on_plane = (vertices[tris[i].a]);
+            __private vector point_minus_direction = ppminus(&point_on_plane, &(ray_cast.pos));
+            // calculate distance to plane
             float d = pgdot(&point_minus_direction, &(tris[i].normal)) /
                       pgdot(&(ray_cast.dir), &(tris[i].normal));
-
+            // this is the new closest triangle
             if (d >= 0 && d < closest_hit) {
                 closest_hit = d;
-                closest_triangle = 1;
+                closest_triangle = i;
             }
         }
     }
-    //__global vector v1_global = ;
-    //__private vector v1 = {v1_global.x, v1_global.y, v1_global.z};
+    triangle tri = tris[closest_triangle];
+    material mat = materials[tri.mat];
+    vector origin = camera->position;
+    vector travel_vec = pCmult(&(ray_cast.dir), closest_hit);
+    vector intersection_point = ppsum(&origin, &travel_vec);
     if (closest_triangle != -1) {
-        out[global_id].r = 255;
-        out[global_id].g = 255;
-        out[global_id].b = 255;
+        //get ambient light
+        __private vector ambient_and_world = gpmult(&(world->world_ambient_color), &(mat.ambient_color));
+        __private vector light = pCmult(&ambient_and_world, world->world_ambient_intensity);
+        //get ambient & spectral light
+        for (int i = 0; i < world->num_lights; ++i) {
+            __private vector light_color = lights[i].color;
+            __private vector light_pos = lights[i].position;
+            __private vector to_light = ppminus(&light_pos, &intersection_point);
+            pnormalize(&to_light);
+
+            if (global_id == 100 * 100-1) {
+                printf("light color %f %f %f", light_color.x, light_color.y, light_color.z);
+                printf("dot %f", fmax(ppdot(&tri.normal, &to_light), 0));
+                printf("mat color %f %f %f", mat.diffuse_color.x, mat.diffuse_color.y, mat.diffuse_color.z);
+
+            }
+            vector diffuse = pCmult(&light_color, fmax(ppdot(&tri.normal, &to_light), 0));
+            diffuse = ppmult(&mat.diffuse_color, &diffuse);
+            if (global_id == 100 * 100-1) {
+                printf("diffuse %f %f %f", diffuse.x, diffuse.y, diffuse.z);
+                printf("light %f %f %f", light.x, light.y, light.z);
+
+            }
+            light = ppsum(&diffuse, &light);
+            if (global_id == 100 * 100-1) {
+                printf("light %f %f %f", light.x, light.y, light.z);
+            }
+        }
+
+        out[global_id].r = light.x * 255;
+        out[global_id].g = light.y * 255;
+        out[global_id].b = light.z * 255;
     }
+
 
 
 
